08852f5ab2fe88e87b450764d6ead0f73a4164f4
Prashant Thakur <prashantst33@yahoo.in>
2021-05-17 17:01:45 +0530

    encap

---
+++ Encapsulation.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+class YoutubeChannel
+{
+private:
+    string name;
+    string ownerName;
+    int subscriberCount;
+    vector<string> PublishedVideoTitles;
+
+public:
+    //we create public methods to access the private var
+    YoutubeChannel(string n, string o)
+    {
+        name = n;
+        ownerName = o;
+        subscriberCount = 0;
+    }
+    void getInfo()
+    {
+        cout << "Name:" << name << endl;
+        cout << "Owner:" << ownerName << endl;
+        cout << "Total subscribers:" << subscriberCount << endl;
+        cout << "Published videos:" << endl;
+        for (auto s : PublishedVideoTitles)
+            cout << s << endl;
+    }
+    void Subscribe()
+    {
+        subscriberCount++;
+    }
+    void Unsubscribe()
+    {
+        if (subscriberCount > 0)
+            subscriberCount--;
+    }
+    void publishVideo(string title)
+    {
+        PublishedVideoTitles.push_back(title);
+    }
+};
+
+int main()
+{
+    YoutubeChannel ytChannel("CodeBeast", "Prashant Singh Thakur");
+    YoutubeChannel ytChannel2("LofiBeats", "Marshmello");
+    ytChannel.getInfo();
+    cout << endl;
+    ytChannel2.getInfo();
+}
